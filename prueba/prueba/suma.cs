b1dbeb015f283533e8dd0affac9140607c1b882a
itzmefabry <rasf080924hgtnrba0@cbtis217.edu.mx>
2025-12-05 00:13:44 -0600

    Plantilla

---
+++ prueba/prueba/suma.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Fabry UwU
+ * Date: 04/12/2025
+ * Time: 08:13 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace prueba
+{
+	/// <summary>
+	/// Description of suma.
+	/// </summary>
+	public partial class suma : Form
+	{
+		public suma()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
