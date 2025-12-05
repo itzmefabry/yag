ead3c2b3417ccc8a9bda0aa3fd7c537de283d003
itzmefabry <rasf080924hgtnrba0@cbtis217.edu.mx>
2025-12-05 00:31:10 -0600

    Suma

--- prueba/prueba/suma.Designer.cs
+++ prueba/prueba/suma.Designer.cs
@@ -36,12 +36,47 @@ namespace prueba
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// hScrollBar1
+			// 
+			this.hScrollBar1.Location = new System.Drawing.Point(81, 58);
+			this.hScrollBar1.Name = "hScrollBar1";
+			this.hScrollBar1.Size = new System.Drawing.Size(80, 26);
+			this.hScrollBar1.TabIndex = 0;
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(43, 124);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(200, 100);
+			this.panel1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(53, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "label1";
 			// 
 			// suma
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "suma";
+			this.ClientSize = new System.Drawing.Size(278, 244);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.hScrollBar1);
 			this.Name = "suma";
+			this.Text = "suma";
+			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.HScrollBar hScrollBar1;
 	}
 }
